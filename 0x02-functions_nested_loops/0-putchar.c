1db1a9616e36891d22a4bf5b0da5a656a7ec8a92
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-03-15 19:28:48 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
 #include "main.h"
-/**
+/*
  * main - entry block
- * @void: no argument
+ *
  * Return: 0
- **/
-int main(void)
-{
-	char str[] = "_putchar";
+ */
+ int main(void)
+ {
+	 char str[] = "_putchar";
+	 int c;
 
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(str[c]);
-	}
-	_putchar('\n');
-	return (0);
-}
+	 for (c = 0; c < 8; c++)
+	 {
+		 _putchar(str[c]);
+	 }
+	 _putchar('\n');
+	 return (0);
+ }
